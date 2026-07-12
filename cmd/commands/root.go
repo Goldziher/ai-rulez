214462c44584d0816e197d6aa01bd5f159ecc0a1
Na'aman Hirschfeld <nhirschfeld@gmail.com>
2026-07-12 10:24:46 +0200

    chore(release): v4.9.0

--- cmd/commands/root.go
+++ cmd/commands/root.go
@@ -14,7 +14,7 @@ import (
 var (
 	cfgFile  string
 	gitToken string
-	Version  = "4.8.0"
+	Version  = "4.9.0"
 )
 
 var RootCmd = &cobra.Command{
--- internal/hooks/setup.go
+++ internal/hooks/setup.go
@@ -13,7 +13,7 @@ const (
 	preCommitSystem       = "pre-commit"
 	huskySystem           = "husky"
 	officialPreCommitRepo = "https://github.com/Goldziher/ai-rulez"
-	officialPreCommitRev  = "v4.8.0"
+	officialPreCommitRev  = "v4.9.0"
 	keyRepo               = "repo"
 	keyHooks              = "hooks"
 	binaryAIRulez         = "ai-rulez"
